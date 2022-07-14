f955733ed5a076a7a6a173adb4b50f930f68da14
connorhoelting <67876305+connorhoelting@users.noreply.github.com>
2022-07-14 17:04:12 -0500

    Added multiple features
    Added:
    Frontend dropdown menu to specify OR/AND/NOT, filtering symbols input box, and to choose to list by alphabetical, most frequently accessed order, or per sponsor payment.
    OR/AND/NOT Search
    Filtering search input based on user input.
    Listing by alphabetical, most frequently accessed order or per sponsor payment.
    
    Things left to do:
    Web Crawler, Autofill, Pagenation

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const path = require('path');
 const app = express();
 const port = 3000;
+
+app.use(express.urlencoded({extended : true}));  
+app.use(express.json());
 
 const url = "mongodb+srv://connor:OOADProject6359@expertsearch.vx8cj.mongodb.net/webengine?retryWrites=true&w=majority";
 const connectionParams={
     useNewUrlParser: true,
     useUnifiedTopology: true 
 }
+mongoose.set('debug', true);
 mongoose.connect(url,connectionParams)
     .then( () => {
         console.log('Connected to the database ')
@@ -21,7 +26,7 @@ db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
   console.log("Connected successfully");
 });
-const seSchema= new mongoose.Schema({ websiteurl: { type: String, required: true }, description: { type: String, required: true}},{collection : 'websites'});
+let seSchema= new mongoose.Schema({ websiteurl: { type: String, required: true }, title: {type: String, required: true},description: { type: String, required: true}, numAccessed: { type: Number, required: true }, sponserPayment: { type: Number, required: true}, dateCreated:{ type: Date, required: true}},{ strict: false },{collection : 'websites'});
 var Websites = mongoose.model('Websites',seSchema);
 
 const staticPath = path.join(__dirname, "public")
@@ -35,11 +40,72 @@ app.get('/search',(req,res) => {
 })
 app.get('/getdata', async (req,res)=>{
   
-   let query =  req.query.query;     
-   var q = Websites.find({$or: [{'websiteurl': {"$regex": query}}, {'description': {"$regex": query}}]}, function(err, doc){ res.send(doc);})
+   
+   let query =  req.query.query; 
+   let typeSearch = req.query.type;
+   let orderSearch = req.query.order;
+   let numOfResultsPerPage = req.query.numOfResultsPerPage;
+   if(typeSearch == "AND")      // AND
+   {
+        if(orderSearch == "ALPHABETICAL")
+        {
+            Websites.find({$or: [{'websiteurl': {"$regex": query}}, {'description': {"$regex": query}}]}, function(err, doc){ res.send(doc);}).sort({title: 'asc'});
+        }
+        else if(orderSearch == "FREQUENTLYACCESSED")
+        {
+            Websites.find({$or: [{'websiteurl': {"$regex": query}}, {'description': {"$regex": query}}]}, function(err, doc){ res.send(doc);}).sort({numAccessed: -1});
+        }
+        else
+        {
+            Websites.find({$or: [{'websiteurl': {"$regex": query}}, {'description': {"$regex": query}}]}, function(err, doc){ res.send(doc);}).sort({sponserPayment: -1});
+        }        
+   }
+   else if (typeSearch == "OR")     // OR           
+   {
+        let queryArray = query.split(" ");          
+        let allqueries = queryArray.join("|");      // Found method googling MongoDB query $in with regex array of element
+        
+        if(orderSearch == "ALPHABETICAL")
+        {
+            Websites.find({$or: [{'websiteurl': {"$regex": allqueries}}, {'description': {"$regex": allqueries}}]}, function(err, doc){ res.send(doc);}).sort({title: 'asc'});
+        }
+        else if(orderSearch == "FREQUENTLYACCESSED")
+        {
+            Websites.find({$or: [{'websiteurl': {"$regex": allqueries}}, {'description': {"$regex": allqueries}}]}, function(err, doc){ res.send(doc);}).sort({numAccessed: -1});
+        }
+        else
+        {
+            Websites.find({$or: [{'websiteurl': {"$regex": allqueries}}, {'description': {"$regex": allqueries}}]}, function(err, doc){ res.send(doc);}).sort({sponserPayment: -1});
+        }    
+   }
+   else                            // NOT
+   {
+       if(orderSearch == "ALPHABETICAL")
+        {
+            Websites.find({$nor: [{'websiteurl': {"$regex": query}}, {'description': {"$regex": query}}]}, function(err, doc){ res.send(doc);}).sort({title: 'asc'});
+        }
+        else if(orderSearch == "FREQUENTLYACCESSED")
+        {
+            Websites.find({$nor: [{'websiteurl': {"$regex": query}}, {'description': {"$regex": query}}]}, function(err, doc){ res.send(doc);}).sort({numAccessed: -1});
+        }
+        else
+        {
+            Websites.find({$nor: [{'websiteurl': {"$regex": query}}, {'description': {"$regex": query}}]}, function(err, doc){ res.send(doc);}).sort({sponserPayment: -1});
+        }    
+   }
+   
    
 })
+app.post('/update',async (req,res) =>{
 
+    
+    console.log(req.body.updateurl);
+    let url = req.body.updateurl;
+  
+    Websites.findOneAndUpdate({websiteurl: url}, {$inc : {'numAccessed': 1}}, null,function(err, doc) {  if (err) return res.send(500, {error: err}); return res.send('Succesfully saved.');
+    });
+    
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
     //let query = "amc";
--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,24 +1,58 @@
+var searchstr;
+var filteredsearchstr;
+var typeSearch;
+var numOfResultsPerPage;
+var filterSymbols;
+var orderSearch;
+function changeSearch(){
+    searchstr = document.getElementById('navBarSearchBar').value;
+    typeSearch = document.getElementById('typeSearch').value;
+    numOfResultsPerPage = document.getElementById('numOfResultsPerPage').value;
+    filterSymbols = document.getElementById('symbolfilter').value;
+    orderSearch = document.getElementById('orderSearch').value;
+
+    removeSymbols();
+    filter();
+    return false;
+}
+function removeSymbols(){
+
+    let rmspaces = filterSymbols.replaceAll(/\s{2,}/g,' ');
+    rmspaces = rmspaces.split(' ');
+    filteredsearchstr = searchstr;
+    rmspaces.forEach(element => {
+        
+        filteredsearchstr = filteredsearchstr.replaceAll(element, "");
+    });
+    filteredsearchstr = filteredsearchstr.trim();
+}
 function filter(){
     
+    
+
     var cardcontainer = document.getElementById('card-container');
-    var search = document.getElementById('navBarSearchBar').value;
+    
     
     cardcontainer.innerHTML="";
     
     let cards = document.createElement('div');
     cards.className ='container-fluid';
-    //console.log(search);        // Search is correctly allocated
+
     $.ajax({ 
         url: '/getdata', 
         method: 'GET',
         dataType: 'json',
         data:
         {
-            query: search      
+            query: filteredsearchstr,
+            type: typeSearch,
+            order: orderSearch,
+            resultsperPage: numOfResultsPerPage,     
         }
      })
     .then(function(data)
     {
+        
         data.forEach(element => {
             let card = document.createElement('div');
                 card.className = 'row';
@@ -28,44 +62,41 @@ function filter(){
         });
         cardcontainer.appendChild(cards);
     })
-    /*
-    fetch('./public/text/database.txt').then(response => response.text()).then(text =>{
-        
-        let db = text.split('\r\n');
-        console.log(db);
-        for(var i = 0; i < db.length; i++)
-        {
-            if(db[i].includes(search))
-            {
-                let entry = db[i].split(' ');
-                let url = entry.pop();
-                let arr = url.split('https://')
-                let short_url = arr.slice(1).toString();
-                let description = entry.toString().replaceAll(',', ' ');
-                let card = document.createElement('div');
-                card.className = 'row';
-                card.innerHTML = '<div class="card-body"><h1 class="card-title"><a href="' + url + '">' + short_url + '</a></h1><p class="card-subtitle">' + url + '</p><p class="card-text">' + description + '</p>'
-                
-                cards.appendChild(card);
-                
-                
-            }
-        }
-        cardcontainer.appendChild(cards);
-    });
-    */
+    
     return false;
 }
 
     
 
 window.addEventListener('load', () => {
-    searchstr = sessionStorage.getItem('str');   
+    
+    $('#card-container').on("click", 'a', function(event){
+        
+        var urls = {updateurl: event.currentTarget.innerHTML}
+        console.log("Card innerHTML = " + event.currentTarget.innerHTML);
+        console.log("Test string = https://www.amctheatres.com")
+        $.post("/update",
+         {
+            updateurl: event.currentTarget.innerHTML
+            
+         },
+         function (response, status) {
+            console.log(response);
+         });
+         
+    })
+    searchstr = sessionStorage.getItem('str');
+    typeSearch = sessionStorage.getItem('typeSearch');
+    numOfResultsPerPage = sessionStorage.getItem('numOfResultsPerPage');
+    filterSymbols = sessionStorage.getItem('filterSymbols');   
+    orderSearch = sessionStorage.getItem('orderSearch');
     document.getElementById('navBarSearchBar').value = searchstr;
+    document.getElementById('typeSearch').value = typeSearch;
+    document.getElementById('numOfResultsPerPage').value = numOfResultsPerPage;
+    document.getElementById('symbolfilter').value = filterSymbols;
+    document.getElementById('orderSearch').value = orderSearch;
+    removeSymbols();
     filter();
-    document.getElementById("searchbtn").addEventListener("click", function(event){
-        event.preventDefault();
-        filter();
-      });
+    
 })
 
